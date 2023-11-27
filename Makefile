eec52c4ec18effb5bb41ab4b7df89149c2b9cb3e
Ron Peters <rpeters@sendhub.com>
2023-11-27 11:28:29 -0500

    updated buld scripts

--- Makefile
+++ Makefile
@@ -1,24 +1,24 @@
 deps-dev:
-	sudo pip2 install -r requirements_dev
+	sudo pip3 install -r requirements_dev
 
 test: clean
-	python2 -m rabbitmqalert.tests.test_apiclient -b; \
-	python2 -m rabbitmqalert.tests.test_argumentsparser -b; \
-	python2 -m rabbitmqalert.tests.test_conditionchecker -b; \
-	python2 -m rabbitmqalert.tests.test_logger -b; \
-	python2 -m rabbitmqalert.tests.test_notifier -b; \
-	python2 -m rabbitmqalert.tests.test_rabbitmqalert -b; \
-	python2 -m rabbitmqalert.tests.models.test_argument -b;
+	python3 -m rabbitmqalert.tests.test_apiclient -b; \
+	python3 -m rabbitmqalert.tests.test_argumentsparser -b; \
+	python3 -m rabbitmqalert.tests.test_conditionchecker -b; \
+	python3 -m rabbitmqalert.tests.test_logger -b; \
+	python3 -m rabbitmqalert.tests.test_notifier -b; \
+	python3 -m rabbitmqalert.tests.test_rabbitmqalert -b; \
+	python3 -m rabbitmqalert.tests.models.test_argument -b;
 
 test-install:
-	sudo python2 setup.py install
+	sudo python3 setup.py install
 
 clean:
 	rm -rf build/ dist/ rabbitmq_alert.egg-info/ .eggs/
-	find . -name *.pyc -delete
+	find . -name '*.pyc' -delete
 
 dist: clean
-	python2 setup.py sdist
+	python3 setup.py sdist
 
 dist-inspect:
 	tar -tvf dist/*
@@ -33,5 +33,6 @@ test-publish: dist
 	twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 
 test-metadata: clean
-	python2 setup.py check --restructuredtext; \
-	python2 setup.py checkdocs
+	python3 setup.py check --restructuredtext; \
+	python3 setup.py checkdocs
+
--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-#! /usr/bin/python2
-
 from setuptools import setup, find_packages
 from os import path
 
 # remember to push a new tag after changing this!
-VERSION = "1.9.0"
+VERSION = "3.0.0"
 
 DIST_CONFIG_PATH = "rabbitmqalert/config"
 
@@ -15,7 +13,8 @@ DATA_FILES = [
 
 
 def generate_readme():
-    return open("README.rst").read()
+    with open("README.rst", "r") as file:
+        return file.read()
 
 
 def generate_data_files():
@@ -33,10 +32,10 @@ setup(
     long_description=generate_readme(),
     packages=find_packages(exclude=["*tests*"]),
     description="Send notifications when predefined conditions are met",
-    author="Germano Fronza (gfronza), Kostas Milonas (mylk), velika12, Robert Kopaczewski (23doors), Ivan Timeev (TeslA1402), Anderson Diego Kulpa Fachini (anderson-fachini), Dawid Deregowski (Venomen)",
-    author_email="germano.inf@gmail.com",
-    url="https://github.com/gfronza/rabbitmq-alert",
-    download_url="https://github.com/gfronza/rabbitmq-alert/tarball/" + VERSION,
+    author="Germano Fronza (gfronza), Kostas Milonas (mylk), velika12, Robert Kopaczewski (23doors), Ivan Timeev (TeslA1402), Anderson Diego Kulpa Fachini (anderson-fachini), Dawid Deregowski (Venomen), Ronald Peters (rpeters1)",
+    author_email="rpeters@sendhub.com",
+    url="https://github.com/rpeters1/rabbitmq-alert-python3",
+    download_url="https://github.com/rpeters1/rabbitmq-alert-python3/tarball/" + VERSION,
     keywords=["rabbitmq", "alert", "monitor"],
     classifiers=[],
     entry_points={
